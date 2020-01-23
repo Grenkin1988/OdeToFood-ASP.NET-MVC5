aeea480fcbe8c0b9a0f25f50a181dcc02a493fd8
grenkin1988 <kruglovgleb@gmail.com>
2020-01-23 18:33:12 +0100

    Etity without nuget

---
+++ OdeToFood.Data/Services/OneToFoodDbContext.cs
@@ -0,0 +1,12 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Data.Services {
+    public class OneToFoodDbContext {
+        public List<Restaurant> Restaurants { get; internal set; }
+
+        internal void SaveChanges() {
+        }
+    }
+}
---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData {
+        private readonly OneToFoodDbContext _db;
+
+        public SqlRestaurantData(OneToFoodDbContext db) {
+            _db = db;
+        }
+
+        public void Add(Restaurant restaurant) {
+            _db.Restaurants.Add(restaurant);
+            _db.SaveChanges();
+        }
+
+        public Restaurant Get(int id) {
+            return _db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll() {
+            return _db.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public void Update(Restaurant restaurant) {
+            var entry = _db.Entry(restaurant);
+            entry.State = EntryState.Modified;
+            _db.SaveChanges();
+        }
+    }
+}
